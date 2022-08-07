844e69446c99316fa395552ca8d836619ddb2e7b
Siddharth Lokasani <110782232+slokasani-thedataguy@users.noreply.github.com>
2022-08-07 14:06:24 -0400

    Update streamlit_app.py
    commented my stuff out.
    editing text from course as instructed in course.
    Created another header for special menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,24 @@
 import streamlit
 
-streamlit.title('Learning some new stuff!')
+# streamlit.title('Learning some new stuff!')
 
-streamlit.header('GITHUB')
-streamlit.text('Created account for the first time ever')
-streamlit.text('Needed it for Snowflake\'s DABW Badge')
-streamlit.text('It\'s really confusing!')
-streamlit.text('Will have to alot some time to learn this later since I need it down the line anyway')
+# streamlit.header('GITHUB')
+# streamlit.text('Created account for the first time ever')
+# streamlit.text('Needed it for Snowflake\'s DABW Badge')
+# streamlit.text('It\'s really confusing!')
+# streamlit.text('Will have to alot some time to learn this later since I need it down the line anyway')
 
-streamlit.header('Streamlit.io')
-streamlit.text('Created this for for the badge as well!')
-streamlit.text('Don\'t even know what this was until I read through some high level info')
-streamlit.text('Was never in to App development. But I guess I\'ll take it up soon')
+# streamlit.header('Streamlit.io')
+# streamlit.text('Created this for for the badge as well!')
+# streamlit.text('Don\'t even know what this was until I read through some high level info')
+# streamlit.text('Was never in to App development. But I guess I\'ll take it up soon')
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My Mom\'s New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
