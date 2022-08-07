83b48d57fac0a24c1a2459ddef402c0ec46ea38e
Siddharth Lokasani <110782232+slokasani-thedataguy@users.noreply.github.com>
2022-08-07 13:58:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit
 
 streamlit.title('Learning some new stuff!')
+
+streamlit.header('GITHUB')
+streamlit.text('Created account for the first time ever')
+streamlit.text('Needed it for Snowflake\'s DABW Badge')
+streamlit.text('It\'s really confusing!')
+streamlit.text('Will have to alot some time to learn this later since I need it down the line anyway')
+
+streamlit.header('Streamlit.io')
+streamlit.text('Created this for for the badge as well!')
+streamlit.text('Don\'t even know what this was until I read through some high level info')
+streamlit.text('Was never in to App development. But I guess I\'ll take it up soon')
