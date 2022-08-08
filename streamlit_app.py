4d0fa745124e7aeef65ed4680632c6435d655c16
Siddharth Lokasani <110782232+slokasani-thedataguy@users.noreply.github.com>
2022-08-08 13:43:12 -0400

    Update streamlit_app.py
    adding fruit text box. and acknowledgement output line.

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,6 @@ my_cur.execute("SELECT * from fruit_load_list union SELECT * from fruit_load_lis
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_add)
