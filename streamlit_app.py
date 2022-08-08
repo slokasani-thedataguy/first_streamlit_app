8c0134290562c846cdadc491b401c407b9072c1c
Siddharth Lokasani <110782232+slokasani-thedataguy@users.noreply.github.com>
2022-08-08 17:48:03 -0400

    Update streamlit_app.py
    Maintenenace underway

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-# streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json()) #Writes data to the screen
-
-# Normalized form of json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays normalized json from previous line
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+    streamlit.error()
+  
 #caution! maintenance underway!
 streamlit.stop()
 
